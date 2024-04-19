82ba3129333e28b2a4c5f3a6c63f42f89acdfa8d
Pansaar Kerdpol <pansaar@gmail.com>
2024-04-19 14:39:55 +0700

    Slip Verification Edition

--- pages/slipVerify/index.tsx
+++ pages/slipVerify/index.tsx
@@ -4,6 +4,10 @@ import axios from 'axios';
 const Index = () => {
     const [image, setImage] = useState<string>('');
     const [imageFile, setImageFile] = useState<File | null>(null);
+    const [info, setInfo] = useState<string>('');
+    const [verify, setVerify] = useState(false)
+    const [displayVerify, setDisplayVerify] = useState(false)
+    const [displayImage, setDisplayImage] = useState(false)
 
     const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files[0]) {
@@ -17,12 +21,13 @@ const Index = () => {
 
             reader.readAsDataURL(file);
             setImageFile(file);
+            setDisplayImage(true)
         }
     };
 
     async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
-
+        
         if (imageFile) {
             const formData = new FormData();
             formData.append('file', imageFile);
@@ -35,8 +40,14 @@ const Index = () => {
                     },
                 });
 
+                if(res.status === 200) {
+                    setVerify(true)
+                }
+
                 const data = res.data;
                 console.log(data);
+                setInfo(JSON.stringify(data))
+                setDisplayVerify(true)
 
             } catch (error) {
                 console.error('Error:', error);
@@ -49,10 +60,19 @@ const Index = () => {
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type='file' id='file' name='file' onChange={onImageChange} />
-                <img src={image} height={300} alt="Preview" />
-                <button type='submit'>Submit</button>
+                <div style={{display: 'flex', justifyContent: 'center', border: 'solid 1px lightGrey', margin: 'auto', width: '600px', boxShadow: '1px 1px 2px lightGrey', height: '340px', marginTop: '20px'}}>
+                {displayImage ? <img src={image} height={300} alt="Preview" style={{margin: 'auto auto'}}/>:<h1 style={{fontWeight: '200', opacity: '0.5', textAlign: 'center', alignContent: 'center', color: 'grey'}}>Choose Slip to Verify</h1>}
+                </div>
+                <input type='file' id='file' name='file' onChange={onImageChange} style={{marginTop: '30px'}}/><br />
+                <button type='submit' style={{marginTop: '10px'}}>Submit</button>
             </form>
+            <div style={{display: displayVerify ? 'block': 'none'}}>
+            <h3 style={{fontWeight: '200'}}>Information</h3>
+            <div style={{ border: 'solid 1px', display: 'block', margin: 'auto', textAlign: 'center' }}>
+                <p style={{ fontSize: '10px', textAlign: 'left', display: 'inline-block', maxWidth: '100%', paddingLeft: '5px', paddingRight: '5px' }}>{info}</p>
+            </div>
+            {verify ? <p style={{color: '#800020'}}>This slip is authentic</p> : <p style={{color: '#800020'}}>This slip is not authentic</p>}
+        </div>
         </div>
     );
 };
